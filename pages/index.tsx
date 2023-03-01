5eca30af599dac6ff89944b31efa5dda373f0361
omrimor <omrimor@getbalance.com>
2023-03-01 09:50:49 +0200

    initial scaffolding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,95 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const companies = [
+  { name: "Microsoft", url: "https://www.microsoft.com/" },
+  { name: "Cwt", url: "https://www.cwt.com/" },
+  { name: "Healthy.io", url: "https://www.healthy.io/" },
+  { name: "Lemonade", url: "https://www.lemonade.com/" },
+  { name: "Balance", url: "https://www.balance.com/" },
+];
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="bg-white dark:bg-slate-800 flex min-h-screen flex-col justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Hi, I'm Omri</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex w-full flex-1 flex-col justify-center px-12">
+        <section>
+          <h1 className="text-4xl font-bold text-slate-900 dark:text-white font-serif">
+            Hi, I'm Omri
+          </h1>
+          <p className="text-slate-500 dark:text-slate-400 font-sans">
+            I've been making websites for close to 15 years.
+          </p>
+        </section>
+        <section className="mt-5">
+          <h2 className="text-2xl text-slate-900 dark:text-white font-bold tracking-tight font-serif">
+            Work
+          </h2>
+          <p className="text-slate-500 dark:text-slate-400 font-sans">
+            I started off as a visual designer but have gradually been shifting
+            towards development and code.
+          </p>
+          <p className="text-slate-500 dark:text-slate-400 font-sans break-words">
+            I've had the opportunity to help folks at{" "}
+            {companies.map((company) => (
+              <a
+                key={company.name}
+                className="text-slate-900 dark:text-white hover:underline px-1"
+                href={company.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {company.name}
+              </a>
+            ))}
+          </p>
+        </section>
+        <section className="mt-5">
+          <h2 className="text-2xl text-slate-900 dark:text-white font-bold tracking-tight font-serif">
+            Links
+          </h2>
+          <p className="text-slate-500 dark:text-slate-400 font-sans">
+            Say hi at{" "}
+            <a
+              href="https://www.theschool.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Email
+            </a>
+            Or find me on{" "}
+            <a
+              href="https://www.theschool.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://www.theschool.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.theschool.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dribble
+            </a>
+          </p>
+        </section>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
